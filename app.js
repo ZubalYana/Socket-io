011e489608245abe1d6efb4d27c83639490cdc49
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-07-27 15:51:22 +0300

    write an endpoint to get the main page

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const io = require('socket.io')(http)
 const PORT = 3000
 const path = require('path')
 app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname, 'public', 'index.html')
+})
 app.listen(PORT, ()=>{
     console.log(`Server works on PORT: ${PORT}`)
 })
