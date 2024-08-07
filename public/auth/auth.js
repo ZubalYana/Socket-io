39b983d5ef9d24ba1b56a17ecf8d31caf3ba3ff0
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-08-07 10:35:12 +0300

    set the correct log in and registraion pages opening, add containers, style them and adjust their positions

--- public/auth/auth.js
+++ public/auth/auth.js
@@ -1,39 +1,26 @@
 //navigation and pages changing
-function getQueryParams() {
-    const params = {};
-    window.location.search.substring(1).split('&').forEach(function(param) {
-        const [key, value] = param.split('=');
-        params[key] = decodeURIComponent(value);
-    });
-    return params;
-}
-const params = getQueryParams();
-const action = params['action'];
-if (action === 'login') {
-    $('.logIn').css('display', 'flex');
-} else if (action === 'register') {
-    $('.registraion').css('display', 'flex');
-}
 $('.backArrow').click(()=>{
-    window.location.href = '/';
+    $('.registrationContainer').css('display', 'none');
+    $('.logInContainer').css('display', 'none');
+
 })
 $('#noAccount').click(()=>{
     $('.logIn').css('display', 'none');
     $('.registraion').css('display', 'flex');
 })
 $('#HaveAnAccount').click(()=>{
-    $('.registraion').css('display', 'none');
-    $('.logIn').css('display', 'flex');
+    $('.registrationContainer').css('display', 'none');
+    $('.logInContainer').css('display', 'flex');
 })
 $('#logInCart').click(function() {
-    $('.registraion').css('display', 'none');
-    $('.logIn').css('display', 'flex');
+    $('.registrationContainer').css('display', 'none');
+    $('.logInContainer').css('display', 'flex');
     $('.greeting').css('display', 'none');
 
 });
 $('#registraionCart').click(function() {
-    $('.logIn').css('display', 'none');
-    $('.registraion').css('display', 'flex');
+    $('.logInContainer').css('display', 'none');
+    $('.registrationContainer').css('display', 'flex');
     $('.greeting').css('display', 'none');
 
 })
