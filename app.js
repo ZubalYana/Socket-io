e346be60b0414c9c2859d8e6d410662bf55a7b5a
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-08-07 08:01:03 +0300

    add the login backend logic and udpate the userScheme

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http)
 const PORT = 3000
 const path = require('path')
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'your_jwt_secret';
 const mongoose = require('mongoose');
 mongoose.connect(`mongodb+srv://zubalana0:QFvTLw87WQlJW3cS@cluster0.wrtk5ym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 `)
@@ -15,8 +19,11 @@ app.get('/', (req,res)=>{
     res.sendFile(__dirname, 'public', 'index.html')
 })
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    firstname: { type: String, required: true },
+    lastName: { type: String },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profilePicture: { type: String }
 });
 const User = mongoose.model('User', userSchema);
 
@@ -38,6 +45,27 @@ app.post('/auth/register', async (req, res) => {
     }
 });
 
+//user login
+app.post('/auth/login', async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({ message: 'email and password are required' });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+        return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '1h' });
+    res.cookie('token', token, { httpOnly: true, secure: true });
+    res.status(200).json({ message: 'Logged in successfully' });
+});
 
 app.get('/auth', (req,res)=>{
     res.sendFile(path.join(__dirname, 'public', 'auth', 'auth.html'))
